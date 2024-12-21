ae5ba2c22446e5178fc72cbddd6abd7de92cdbb3
Baltharaaz <bjbruins1@yahoo.com>
2024-12-20 21:31:18 -0800

    Finalized code submission; full runtime implemented and knowledge base constructed

--- knowledge_base.py
+++ knowledge_base.py
@@ -16,15 +16,19 @@ def kb_declare():  # Function to create the knowledge base from the .csv file
                 brand = '\'' + row[7].replace('\'', '') + '\''  # Brand is its own fact about a model we would want
                 # to let the user query
                 f.write("phone(" + entry + ").\n") # Write the format to .pl file. Will use pyswip in final
+                Prolog.assertz("phone(" + entry + ")")
                 f.write("brand(" + entry + "," + brand + ").\n")
+                Prolog.assertz("brand(" + entry + "," + brand + ")")
             else:
                 continue
 
             storage = row[5]  # Begin handling storage section
             if isinstance(storage, str):  # Make sure we do not receive nan
                 match = re.search("((\d+)?/?)+\s*([gm]b)?|\d+\s*([gm]b)?\s*/\d+\s*([gm]b)?", storage.lower())
-                if match:
-                    if len(match.group(0).split("/")) > 1:
+                # Regex to search the entries for the correct storage parameters and convert to storable format
+                if match: # If we find the pattern
+                    if len(match.group(0).split("/")) > 1: # Split the storage options from each other and insert into
+                        # the KB
                         for storage_size in match.group(0).split("/"):
                             if "mb".lower() in storage.lower():
                                 storage_size = storage_size.split(" ")[0]
@@ -35,7 +39,7 @@ def kb_declare():  # Function to create the knowledge base from the .csv file
                                 storage_size = storage_size.split(" ")[0]
                                 f.write("storage(" + entry + "," + storage_size + ").\n")
                                 Prolog.assertz("storage(" + entry + "," + storage_size + ")")
-                    else:
+                    else: # If there's only one option
                         if "mb".lower() in storage.lower():
                             storage = storage.split(" ")[0]
                             storage = float(storage) / 1024
@@ -46,7 +50,7 @@ def kb_declare():  # Function to create the knowledge base from the .csv file
                             f.write("storage(" + entry + "," + storage + ").\n")
                             Prolog.assertz("storage(" + entry + "," + str(storage) + ")")
 
-            ram = row[43]  # Handle ram
+            ram = row[43]  # Handle ram and input into KB
             if isinstance(ram, str):
                 match = re.findall("\d+/?\d* [gm]b ram", ram.lower())
                 if match:
@@ -62,7 +66,7 @@ def kb_declare():  # Function to create the knowledge base from the .csv file
                                 f.write("ram(" + entry + "," + str(ram) + ").\n")
                                 Prolog.assertz("ram(" + entry + "," + str(ram) + ")")
 
-            cpu = row[8]  # Handle CPU
+            cpu = row[8]  # Handle CPU and input into KB
             if isinstance(cpu, str):
                 match = re.search("(\d+\.?\d*)\s*[gm]hz", cpu.lower())
                 if match:
@@ -78,21 +82,20 @@ def kb_declare():  # Function to create the knowledge base from the .csv file
 
 
 
-            camera = row[3]
+            camera = row[3]  # Handle camera and input into KB
             if isinstance(camera, str):
                 match = re.search("(\d+\.*\d*)\s*mp", camera.lower())
                 if match:
                     camera = match.group(1)
                     f.write("camera(" + entry + "," + str(camera) + ").\n")
                     Prolog.assertz("camera(" + entry + "," + str(camera) + ")")
-                else:
+                else:  # Any entry without a MP listing will be assumed to have no camera functionality
                     camera = 0
                     f.write("camera(" + entry + "," + str(camera) + ").\n")
                     Prolog.assertz("camera(" + entry + "," + str(camera) + ")")
 
-            usb = row[6]  # Handle usb
 
-            bluetooth = row[23]
+            bluetooth = row[23] # Handle bluetooth
 
             if isinstance(bluetooth, str):
                 if "no".lower() in bluetooth.lower():
@@ -108,49 +111,113 @@ def kb_declare():  # Function to create the knowledge base from the .csv file
             if isinstance(gps, str):
                 if "yes".lower() in gps.lower():
                     gps = "yes"
-                    f.write("gps(" + entry + "," + gps + ").\n")
-                    Prolog.assertz("gps(" + entry + "," + gps + ")")
+                    f.write("gps(" + entry + ").\n")
+                    Prolog.assertz("gps(" + entry + ")")
                 else:
                     gps = "no"
                     f.write("gps(" + entry + "," + gps + ").\n")
                     Prolog.assertz("gps(" + entry + "," + gps + ")")
 
+            volume = row[20]  # Handle device dimensions
+            if isinstance(volume, str):
+                match = re.search("\((\d+\.*\d*) *x *(\d+\.*\d*) *x *(\d+\.*\d*) in\)", volume.lower())
+                if match:
+                    length = match.group(1)
+                    height = match.group(2)
+                    width = match.group(3)
+                    if isinstance(length, str) and isinstance(height, str) and isinstance(width, str):
+                        f.write("dimensions( " + entry + "," + length + "," + height + "," + width + ").\n")
+                        Prolog.assertz("dimensions( " + entry + "," + length + "," + height + "," + width + ")")
 
-    # print(list(Prolog.query("bluetooth(X, no)")))
+            resolution = row[10] # Handle screen size/resolution (it says resolution in the dataset but it's size)
+            if isinstance(resolution, str):
+                match = re.search("(\d+\.*\d*)\s*inches", resolution.lower())
+                if match:
+                    screen_size = match.group(1)
+                    if isinstance(screen_size, str):
+                        f.write("screen_size(" + entry + "," + screen_size + ")\n")
+                        Prolog.assertz("screen_size(" + entry + "," + screen_size + ")")
 
-    # Begin rule declaration
+    # Begin rule declaration; many rules are utilized in other rules (particularly the greater/lesser selection rules)
+    #   to constrain their results.
 
-    Prolog.assertz("good_cpu(X) :- " 
+    Prolog.assertz("good-cpu(X) :- "
+                   "phone(X),"
                    "cpu(X, Value), "
                    "Value >= 1")
-    Prolog.assertz("choose_cpu(X, Y) :- "
+    Prolog.assertz("greater-cpu(X, Y) :- "
+                   "phone(X),"
                    "cpu(X, Value), "
                    "Value >= Y")
-    Prolog.assertz("good_ram(X) :- "
+    Prolog.assertz("lesser-cpu(X, Y) :- "
+                   "phone(X),"
+                   "cpu(X, Value), "
+                   "Value < Y")
+    Prolog.assertz("good-ram(X) :- "
+                   "phone(X),"
                    "ram(X, Value), "
                    "Value >= 1")
-    Prolog.assertz("choose_ram(X, Y) :- "
+    Prolog.assertz("greater-ram(X, Y) :- "
+                   "phone(X),"
                    "ram(X, Value), "
-                   "Value >= 6")
-    Prolog.assertz("choose_camera(X, Y) :- "
+                   "Value >= Y")
+    Prolog.assertz("lesser-ram(X, Y) :- "
+                   "phone(X),"
+                   "ram(X, Value), "
+                   "Value < Y")
+    Prolog.assertz("greater-storage(X, Y) :-"
+                   "phone(X),"
+                   "storage(X, Value), "
+                   "Value >= Y")
+    Prolog.assertz("lesser-storage(X, Y) :- "
+                   "phone(X),"
+                   "storage(X, Value), "
+                   "Value < Y")
+    Prolog.assertz("greater-width(X, Y) :- "
+                   "phone(X),"
+                   "dimensions(X, Length, Height, Width), "
+                   "Width >= Y")
+    Prolog.assertz("lesser-width(X, Y) :- "
+                   "phone(X),"
+                   "dimensions(X, Length, Height, Width), "
+                   "Width < Y")
+    Prolog.assertz("greater-screen(X, Y) :- "
+                   "phone(X),"
+                   "screen_size(X, Value), "
+                   "Value >= Y")
+    Prolog.assertz("lesser-screen(X, Y) :- "
+                   "phone(X),"
+                   "screen_size(X, Value), "
+                   "Value < Y")
+    Prolog.assertz("greater-camera(X, Y) :- "
+                   "phone(X),"
                    "camera(X, Value), "
                    "Value >= Y")
+    Prolog.assertz("lesser-camera(X, Y) :- "
+                   "phone(X),"
+                   "camera(X, Value), "
+                   "Value < Y")
+    Prolog.assertz("multimedia(X) :-"
+                   "phone(X),"
+                   "greater-camera(X, 10), "
+                   "greater-storage(X, 24)")
     Prolog.assertz("high-end(X) :- "
-                   "good_cpu(X), "
-                   "good_ram(X), "
-                   "choose_camera(X, 1)")
-    #Prolog.assertz("modern(X) :- "
-    #               "high-end(X), ")
-    print(list(Prolog.query("high-end(X)")))
+                   "phone(X),"
+                   "good-cpu(X), "
+                   "good-ram(X), "
+                   "greater-camera(X, 5)")
+    Prolog.assertz("modern(X) :- "
+                   "high-end(X), "
+                   "lesser-width(X, 0.5), "
+                   "greater-storage(X, 16)")
+    Prolog.assertz("old-school(X) :- "
+                   "phone(X), "
+                   "gps(X, no), "
+                   "greater-width(X, 0.6), "
+                   "lesser-ram(X, 2), "
+                   "lesser-cpu(X, 1)")
 
-            # length = row[]  # Handle device dimensions
-            # height = row[]
-            # width = row[]
 
-            # availability = row[]
-
-
-            # Begin rule construction
 
 def kb_load():  # Function to consult constructed knowledge in existing Prolog file.
     Prolog.consult('knowledge_base.pl')
--- main.py
+++ main.py
@@ -12,8 +12,9 @@ def clear_screen():
 
 def runtime():
     kb.kb_declare()
-    while True:
-        running = True
+    running = True
+    clear_screen()
+    while running:
         choice = input("Welcome to the Phone Recommendation System! \n"
                        "Please select from the following options:\n"
                        "1. Get Phone by Classification\n"
@@ -26,35 +27,93 @@ def runtime():
                                    "2. Good RAM\n"
                                    "3. High End Phone\n"
                                    "4. Modern Phone\n"
-                                   "5. \n"
-                                   "6. \n"
-                                   "7. \n"
-                                   "8. \n"
-                                   "9. \n"
-                                   "10. \n")
-                match phone_type:
+                                   "5. Old School\n"
+                                   "6. Multimedia\n"
+                                   "7. Large Screen\n"
+                                   "8. Small Screen\n"
+                                   "9. Return to Main Menu\n"
+                                   "10. Exit Application\n")
+                match phone_type: # Runtime for the application; results are sorted alphabetically
                     case "1":
-                        results = Prolog.query("good_cpu(X)")
-                        sorted_results = sorted(results, key=lambda result: result['X'])
+                        results = Prolog.query("good-cpu(X)")
+                        sorted_results = sorted(results, key=lambda result: result['X'].casefold())
                         for result in sorted_results:
-                            print(result["X"] + '\n')
-                            input("These phones are classified as having a good CPU due to their CPU being 1"
-                                  "or faster\n"
-                                  "Press Enter to return to Main Menu")
-                        break
+                            print(result["X"])
+                        input("\nThese phones are classified as having a good CPU due to their CPU being 1 GHz "
+                              "or faster!\n\n"
+                              "Press Enter to return to Main Menu")
                     case "2":
-                        break
+                        results = Prolog.query("good-ram(X)")
+                        sorted_results = sorted(results, key=lambda result: result['X'].casefold())
+                        for result in sorted_results:
+                            print(result["X"])
+                        input("\nThese phones are classified as having good RAM due to having at or over 4 GB of RAM!\n\n"
+                             "Press Enter to return to Main Menu")
                     case "3":
-                        break
+                        results = Prolog.query("high-end(X)")
+                        sorted_results = sorted(results, key=lambda result: result['X'].casefold())
+                        for result in sorted_results:
+                            print(result["X"])
+                        input("\nThese phones are classified as \"high end\" due to fulfilling the requirements\n"
+                              "for good RAM (>=4GB) and a good CPU (>=1GHz). Additionally, these phones have cameras with \n"
+                              "at or over 5 megapixels of quality; this number is chosen due to the age of this dataset.\n\n"
+                              "Press Enter to return to Main Menu")
+                    case "4":
+                        results = Prolog.query("modern(X)")
+                        sorted_results = sorted(results, key=lambda result: result['X'].casefold())
+                        for result in sorted_results:
+                            print(result["X"])
+                        input("\nThese phones are classified as \"modern\" because they fulfill the requirements\n"
+                              "for a high end phone (>=4GB RAM, >=1GHz CPU, >=5MP camera). Additionally, \n"
+                              "these phones are as slimmer than 0.5 inches and have storage at or over 16 GB.\n\n"
+                              "Press Enter to return to Main Menu")
+                    case "5":
+                        results = Prolog.query("old-school(X)")
+                        sorted_results = sorted(results, key=lambda result: result['X'].casefold())
+                        for result in sorted_results:
+                            print(result["X"])
+                        input("\nThese phones are considered \"old school\" because they lack a GPS feature, have\n"
+                              "CPUs slower than 1 GHz, and have less than 2 GB of Ram. They are also as wide as\n"
+                              "or wider/bulkier than 0.6 inches!\n\n"
+                              "Press Enter to return to Main Menu")
+                    case "6":
+                        results = Prolog.query("multimedia(X)")
+                        sorted_results = sorted(results, key=lambda result: result['X'].casefold())
+                        for result in sorted_results:
+                            print(result["X"])
+                        input("\nThese phones are considered \"multimedia\" because they have a camera at or over 10 megapixels\n"
+                              "and they have over 24GB of storage space!\n\n"
+                              "Press Enter to return to Main Menu")
+                    case "7":
+                        results = Prolog.query("greater-width(X, 0.5)")
+                        sorted_results = sorted(results, key=lambda result: result['X'].casefold())
+                        for result in sorted_results:
+                            print(result["X"])
+                        input("\nThese phones have a screen larger than 5 inches!\n\n"
+                              "Press Enter to return to Main Menu")
+
+                    case "8":
+                        results = Prolog.query("lesser-width(X, 0.4)")
+                        sorted_results = sorted(results, key=lambda result: result['X'].casefold())
+                        for result in sorted_results:
+                            print(result["X"])
+                        input("\nThese phones have a screen smaller than 4 inches!\n\n"
+                              "Press Enter to return to Main Menu")
+
+                    case "9":
+                        pass
+                    case "10":
+                        running = False
+                    case _:
+                        pass
             case "2":
                 running = False
+            case _:
+                pass
 
         clear_screen()
-
-        if not running:
-            break
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    kb.kb_declare()
+    runtime()
